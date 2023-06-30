cc3ee79ac0a2e52ead8125ac8c8fcba9ff7af3bb
Jeremy <jstanley490@gmail.com>
2023-06-29 20:12:54 -0500

    add fetch user to root

--- src/root.jsx
+++ src/root.jsx
@@ -6,12 +6,33 @@ import { Toaster } from "react-hot-toast";
 
 export default function Root() {
   const [token, setToken] = useState("");
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    async function fetchUser() {
+      const localToken = localStorage.getItem("token");
+      if (localToken) {
+        setToken(localToken);
+        const response = await fetch(`${BASE_URL}/users/me`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localToken}`,
+          },
+        });
+        const result = await response.json();
+        if (result.id) {
+          setUser(result);
+        }
+      }
+    }
+    fetchUser();
+  }, [token]);
 
   return (
     <div>
       <Navbar token={token} setToken={setToken} />
       <Toaster position="bottom-center" />
-      <Outlet context={{}} />
+      <Outlet context={{ token, setToken }} />
     </div>
   );
 }
