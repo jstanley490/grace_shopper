bd62d860e17194f1bd8bcbef10582c38d00bff19
Jeremy <jstanley490@gmail.com>
2023-07-01 12:15:58 -0500

    css updates for hero element and login/register components

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,3 +1,10 @@
 export default function HomePage() {
-  return <div id="page">hello world</div>;
+  return (
+    <>
+      <div className="background-img"></div>
+      <div id="page">
+        <h1>hello world</h1>
+      </div>
+    </>
+  );
 }
