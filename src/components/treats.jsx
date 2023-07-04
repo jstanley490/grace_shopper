734ce6a143ca10b2ad1440095cabc00bfb20ef13
Jeremy <jstanley490@gmail.com>
2023-07-04 10:26:54 -0500

    get treats and get merch

--- src/components/treats.jsx
+++ src/components/treats.jsx
@@ -1,3 +1,22 @@
+import { useOutletContext } from "react-router-dom";
+
 export default function Treats() {
-  return <div id="page">treats</div>;
+  const { treats, setTreats } = useOutletContext();
+
+  return (
+    <div id="page">
+      <div className="page-body">
+        <h1>treats</h1>
+        {treats.map((treat) => {
+          // console.log(treat);
+          return (
+            <div className="post" key={treat.id}>
+              <h2>{treat.type}</h2>
+              <p>{treat.price}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
